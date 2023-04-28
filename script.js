df49f00bac5f518dd4f98db91f0525f8062d40f7
Thayna Marins <thayna.mgas@hotmail.com>
2023-04-28 13:17:55 -0300

    atualização tela inicial

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+/* Esconder/mostrar a sidebar */
+var btn_bar = document.querySelector('#sidebar-toggle');
+var sidebar = document.querySelector('#sidebar');
+btn_bar.addEventListener('click', function(){
+    if(sidebar.style.display === 'flex'){
+        sidebar.style.display = 'none';
+    }else{
+        sidebar.style.display = 'flex';
+    }
 
-    console.log("Meu código está sendo executado dentro do bloco $(document).ready()!");
+});
 
-    var btn = document.querySelector('#sidebar-toggle');
-    var sidebar = document.querySelector('#sidebar');
+   
 
-    btn.addEventListener('click', function(){
-        if(sidebar.style.display === 'flex'){
-            sidebar.style.display = 'none';
-        }else{
-            sidebar.style.display = 'flex';
-        }
+ /* Esconder/mostrar opçoes do usuario */
+var btn_user = document.querySelector('#user-action');
+var op_user = document.querySelector('.user-options');
+btn_user.addEventListener('click', function(){
+    if(op_user.style.display === 'none'){
+        op_user.style.display = 'block';
+    }else{
+        op_user.style.display = 'none';
+    }
 
-    });
-
-
-    
+});   
