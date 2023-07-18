a53b75ee35f160ce842e23114e6e2bf47d142fa8
thaynamarinss <thayna.mgas@hotmail.com>
2023-07-18 11:21:34 -0300

    trabconcluido-ligaçãoBDcompleta+uso sessionStorage

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 /*Express gerencia requisições/rotas */
+// nodemon app.js para executar
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const Sequelize = require('sequelize');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+ 
+// Habilitar o middleware CORS
+app.use(cors());
 // Define o caminho para os arquivos estáticos
 app.use(express.static(path.join(__dirname)));
-
 // Middleware para fazer o parsing do corpo das requisições como JSON
 const jsonParser = bodyParser.json();
 // Usando o jsonParser como middleware para todas as rotas
 app.use(jsonParser);
-
-//const db = require('./models/config_db'); /*importando arquivo que faz ligação com o db */
 const User = require('./models/User'); // Importe o modelo do usuário do seu projeto
+const Messages = require('./models/Messages'); // Importe o modelo do usuário do seu projeto
+const Chat = require('./models/Chat');
 
+  
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
 
 app.get('/script.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'script.js'));
@@ -29,23 +36,26 @@ app.get('/script.js', (req, res) => {
   app.post('/login', (req, res) => { 
     var user_id = req.body.user_id;
     var email = req.body.email;
-    var username = req.body.username;
-    //var password = req.body.password;
-    console.log('Username:', username); 
-    console.log('email:', email);
-    console.log('user_id:', user_id);
+    var username = req.body.username; 
+    // console.log('Username:', username); 
+    // console.log('email:', email);
+    // console.log('user_id:', user_id);
 
     User.findOne({ where: { user_id: user_id } })
         .then((user) => {
             if (user) {
                 // Usuário encontrado, retornar uma resposta de login bem-sucedido
-                res.status(200).json({ success: true });
+                res.status(200).json({ success: true });     
+                // Iniciar a sessão do usuário e definir um cookie de autenticação           
+
+
             } else {
                 // Usuário não encontrado, realizar o cadastro
                 User.create({ user_id, email, username })
-                    .then(() => {
+                    .then((createdUser) => {
                         // Usuário cadastrado com sucesso, retornar uma resposta indicando a entrada
                         res.status(200).json({ success: true });
+                        // Usuário cadastrado com sucesso, iniciar a sessão e definir um cookie de autenticação
                     })
                     .catch((error) => {
                         console.error('Erro ao criar usuário:', error);
@@ -59,31 +69,171 @@ app.get('/script.js', (req, res) => {
         });
 });
 
+app.get('/obter-mensagem-aleatoria', (req, res) => {
+  // Consulta o banco de dados para obter uma mensagem aleatória
+  Messages.findAll({
+    attributes: ['message'],
+    where: {
+      is_system_message: 1,
+      user_id: null
+    },
+    order: Sequelize.literal('RAND()'), // Ordenar aleatoriamente no MySQL
+    limit: 1
+  })
+    .then((messages) => {
+      if (messages.length > 0) {
+        console.log('Mensagem aleatória:', messages[0].message);
 
-// app.post('/login', (req, res) => { 
-//     var user_id = req.body.user_id;
-//     var email = req.body.email;
-//     var username = req.body.username;
-//     var password = req.body.password;  //apagar  
-//     //console.log('SE Username:', username); 
-//     //console.log('SE Password:', password);
+        // Adicionar um atraso de 1 segundo antes de enviar a resposta
+        setTimeout(() => {
+          res.status(200).json({ success: true, message: messages[0].message });
+        }, 1000); // 1000 milissegundos = 1 segundo
+      } else {
+        console.log('Nenhuma mensagem encontrada');
+        res.status(404).json({ success: false, message: 'Nenhuma mensagem encontrada' });
+      }
+    })
+    .catch((error) => {
+      console.error('Erro ao obter as mensagens:', error);
+      res.status(500).json({ success: false, message: 'Erro ao obter mensagem aleatória' });
+    });
+});
+
+
+app.post('/criar-chat', async (req, res) => {
+  try {
+    // Extrair os dados necessários do corpo da requisição
+    var user_id = req.body.user_id;
+    var nome_chat =req.body.nome_chat;
     
-//     User.findOne({ where: { username: username, password: password } })
-//         .then((user) => {
-//             if (user) {
-//                 res.status(200).json({ success: true });
-//             } else {
-//                 res.status(401).json({ success: false, message: 'Nome de usuário ou senha inválidos!' });
-//             }
-//         })
-//         .catch((error) => {
-//             console.error('Ocorreu um erro durante a autenticação:', error);
-//             res.status(500).json({ success: false, message: 'Ocorreu um erro durante a autenticação.' });
-//         });
-// });
+    // Criar um novo chat no banco de dados
+    var newChat = await Chat.create({
+      user_id: user_id,
+      nome_chat: nome_chat
+    });
+    console.log("criado chat com id: ", newChat.chat_id);
+    // Retornar uma resposta de sucesso
+    res.status(200).json({ success: true, message: 'Chat criado com sucesso',  chat_id: newChat.chat_id });
+  } catch (error) {
+    // Em caso de erro, retornar uma resposta de erro
+    console.error('Erro ao criar o chat:', error);
+    res.status(500).json({ success: false, message: 'Erro ao criar o chat' });
+  }
+});
+
+app.get('/obter-chats', (req, res) => {
+  // Extrair o ID do usuário dos parâmetros da requisição
+  const  userId  = req.query.user_id;
+  console.log("userid do app obter chat", userId);
+  // Consultar o banco de dados para obter os chats do usuário
+  Chat.findAll({
+    where: {
+      user_id: userId
+    }
+  })
+    .then((chats) => {
+      res.status(200).json({ success: true, chats });
+    })
+    .catch((error) => {
+      console.error('Erro ao obter os chats do banco de dados:', error);
+      res.status(500).json({ success: false, message: 'Erro ao obter os chats do usuário' });
+    });
+});
+
+app.post('/salvar-mensagem', (req, res) => {
+  // Extrair os dados necessários do corpo da requisição
+  const { message, chat_id, user_id, is_system_message } = req.body;
+
+  // Realizar a lógica para salvar a mensagem no banco de dados
+  Messages.create({
+    message: message,
+    chat_id: chat_id,
+    user_id: user_id,
+    is_system_message: is_system_message
+  })
+    .then(() => {
+      res.status(200).json({ success: true, message: 'Mensagem salva com sucesso' });
+    })
+    .catch((error) => {
+      console.error('Erro ao salvar a mensagem:', error);
+      res.status(500).json({ success: false, message: 'Erro ao salvar a mensagem' });
+    });
+});
+  
+
+app.get('/obter-conteudo-chat/:chatId', (req, res) => {
+  const chatId = req.params.chatId;
+
+  // Consultar o banco de dados para obter as mensagens do chat pelo ID, ordenando por message_date
+  Messages.findAll({
+    attributes: ['message', 'is_system_message'],
+    where: {
+      chat_id: chatId
+    },
+    order: [['message_date', 'ASC']] // Ordenar as mensagens pelo campo message_date em ordem ascendente (ASC)
+  })
+    .then((messages) => {
+      if (messages.length > 0) {
+        
+        const conteudoChat = messages.map((message) => {
+          return {
+            message: message.message,
+            is_system_message: message.is_system_message
+          };
+        });
+        console.log("rota object message:", conteudoChat);
+        res.status(200).json({ success: true, conteudo: conteudoChat });
+      } else {
+        console.log('Nenhuma mensagem encontrada para o chat');
+        res.status(404).json({ success: false, message: 'Nenhuma mensagem encontrada para o chat' });
+      }
+    })
+    .catch((error) => {
+      console.error('Erro ao obter as mensagens do chat:', error);
+      res.status(500).json({ success: false, message: 'Erro ao obter o conteúdo do chat' });
+    });
+});
 
 
+app.get('/pegar-nome', (req, res) => {
+  const user_id = req.query.user_id;
 
+  // Use o método findOne do modelo User para buscar o usuário pelo user_id
+  User.findOne({ where: { user_id: user_id } })
+    .then((user) => {
+      if (user) {
+        // Usuário encontrado, retornar o username na resposta JSON
+        res.status(200).json({ success: true, username: user.username });
+      } else {
+        // Usuário não encontrado, retornar uma resposta de erro
+        res.status(404).json({ success: false, message: 'Usuário não encontrado' });
+      }
+    })
+    .catch((error) => {
+      // Erro ao buscar o usuário, retornar uma resposta de erro
+      console.error('Erro ao buscar o usuário:', error);
+      res.status(500).json({ success: false, message: 'Ocorreu um erro ao buscar o usuário' });
+    });
+});
+
+app.post('/clear-convers', (req, res) => {
+  const user_id = req.body.user_id;
+
+  // Deletar todos os registros da tabela "messages" onde "user_id = user_id"
+  Messages.destroy({ where: { user_id: user_id } })
+    .then(() => {
+      // Deletar todos os registros da tabela "chat" onde "user_id = user_id"
+      return Chat.destroy({ where: { user_id: user_id } });
+    })
+    .then(() => {
+      console.log('Conversas deletadas com sucesso');
+      res.status(200).json({ success: true, message: 'Conversas deletadas com sucesso' });
+    })
+    .catch((error) => {
+      console.error('Erro ao deletar conversas:', error);
+      res.status(500).json({ success: false, message: 'Erro ao deletar conversas' });
+    });
+});
 
 app.listen(8080, () => {
     console.log("servidor iniciado na porta 8080")
--- script.js
+++ script.js
@@ -10,11 +10,16 @@ btn_bar.addEventListener('click', function(){
 
 });
 
-   
+/* examples principal */
+function updatePlaceholder(text) {
+  const messageInput = document.getElementById("message-input");
+  messageInput.textContent = text;
+}
+
 
  /* Esconder/mostrar opçoes do usuario */
 
-var btn_user = document.querySelector('#user-action');
+var btn_user = document.querySelector('#btn-profile');
 var op_user = document.querySelector('.user-options');
 btn_user.addEventListener('click', function(){
     if(op_user.style.display === 'none'){
@@ -31,15 +36,18 @@ btn_user.addEventListener('click', function(){
 
 var loginForm = document.querySelector('#login-form');
 var chatScreen = document.querySelector('#chat-screen');
-
+var user_id = null;
+var profile_pic = null;
 /*Botao fazer login com goole*/
 function handleCredentialResponse(response) {
-  const data = jwt_decode(response.credential)
-  user_id = data.sub
-  email = data.email
-  username = data.name  
-  console.log(data)
-  console.log('o email é:',user_id)
+  const data = jwt_decode(response.credential);
+ // console.log(data);
+  user_id = data.sub;
+  email = data.email;
+  username = data.name; 
+  profile_pic = data.picture;  
+  console.log('profile_pic',profile_pic);
+ 
   $.ajax({
     
     url: '/login',
@@ -48,13 +56,16 @@ function handleCredentialResponse(response) {
     data:JSON.stringify({user_id: user_id, email: email, username: username }),
    
     success: function(response) {
-      console.log('Entrou ajax ');
-
+      // Chamar a função para obter os chats do usuário assim que a página for carregada
+      obterChatsDoUsuario(user_id);
       if (response.success) {
+        sessionStorage.setItem('user_id_sessionstorage', user_id);
+        sessionStorage.setItem('profile_pic_sessionstorage', profile_pic);
         // Login bem-sucedido, exibir o chat
-        
+      
         document.getElementById('login-screen').classList.add('hidden');
         chatScreen.classList.remove('hidden');
+        pegar_nomeUser(); 
       } else {
         // Login inválido, exibir mensagem de erro
         alert(response.message);
@@ -64,61 +75,365 @@ function handleCredentialResponse(response) {
       alert('Ocorreu um erro durante a autenticação. Tente novamente');
       //console.log(JSON.stringify(error));
     }
+  }); 
+}
+
+window.onload = function() { 
+
+  // Verifica se o usuário já está logado (se o cookie 'user_id' já existe) 
+ //sessionStorage (permanece ate fechar a aba)
+ user_id = sessionStorage.getItem('user_id_sessionstorage');
+ profile_pic = sessionStorage.getItem('profile_pic_sessionstorage');
+ console.log("user id on load", sessionStorage.getItem('user_id_sessionstorage')) ;
+ if ( user_id != null) {
+    // Caso o cookie 'user_id_cookie' exista, o usuário já está logado,
+    // então exiba o chat imediatamente, evitando a tela de login 
+    document.getElementById('login-screen').classList.add('hidden');
+    chatScreen.classList.remove('hidden');
+    pegar_nomeUser(); 
+    obterChatsDoUsuario(user_id);
+  } else {
+    // Caso o cookie 'user_id_cookie' não exista, o usuário não está logado,
+    // então exiba a tela de login e o botão para fazer login com o Google
+      google.accounts.id.initialize({
+        client_id: "",
+        callback: handleCredentialResponse
+      });
+      google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "filled_black", size: "large", shape: "pill" } // customization attributes
+      );
+      google.accounts.id.prompt(); // also display the One Tap dialog
+  }
+ 
+};
+
+function Sair_user(){
+  sessionStorage.removeItem('user_id_sessionstorage');
+  sessionStorage.removeItem('profile_pic_sessionstorage');
+  location.reload();
+}
+
+function pegar_nomeUser(){
+
+  $.ajax({
+    url: '/pegar-nome',
+    method: 'GET',
+    data: { user_id: user_id },
+    success: function(response) {
+      // Verificar se a requisição foi bem-sucedida
+      if (response.success) {
+        const words = response.username.split(' ');
+        console.log("words get nome", words);
+        // Selecione o elemento da imagem pelo ID "profile-image"
+        const imagemPerfil = document.getElementById("profile-image");
+        // Atribua o valor da URL da imagem ao atributo src
+        imagemPerfil.src = profile_pic;
+        if(words.length >=2){
+          // Se tiver pelo menos duas palavras, pegar as duas primeiras (índices 0 e 1)
+          const firstName = words[0];
+          const lastName = words[1];
+          // Montar o nome com as duas primeiras palavras
+          const name = firstName + ' ' + lastName;
+          // Atualizar o elemento com o nome
+          document.getElementById("username").textContent = name;       
+          
+        } else {
+          // Se tiver apenas uma palavra ou nenhuma, usar o nome completo do username
+          document.getElementById("username").textContent = response.username;
+        }
+
+        console.log('requisição de nome sucedida');
+      } else {
+        console.error('Erro ao pegar username');
+      }
+    },
+    error: function() {
+      console.error('Ocorreu um erro durante a requisição do nome.');      
+    }
   });  
-
-
-
-
-
-
+  
 }
-window.onload = function () {
-  google.accounts.id.initialize({
-    client_id: "",
-    callback: handleCredentialResponse
+
+function clear_Convers() {
+  //const user_id = user_id;
+  $.ajax({
+    url: '/clear-convers',
+    method: 'POST',
+    contentType: 'application/json',
+    data:JSON.stringify({  user_id: user_id }),
+    success: function(response) {
+      // Verificar se a requisição foi bem-sucedida
+      if (response.success) {
+        console.log('Conversas deletadas com sucesso');
+        obterChatsDoUsuario(user_id);
+      } else {
+        console.error('Erro ao deletar conversas:', response.message);
+      }
+    },
+    error: function() {
+      console.error('Ocorreu um erro ao deletar as conversas.');
+    }
   });
-  google.accounts.id.renderButton(
-    document.getElementById("buttonDiv"),
-    { theme: "filled_black", size: "large", shape:"pill"}  // customization attributes
-  );
-  google.accounts.id.prompt(); // also display the One Tap dialog
 }
 
 
-// loginForm.addEventListener('submit', function(event) {
-//   event.preventDefault(); // Impede o envio do formulário
-//   var usernameInput = document.querySelector('#username-input');
-//   var passwordInput = document.querySelector('#password-input');
-//   var username = usernameInput.value;
-//   var password = passwordInput.value;
+// Enviar mensagens do usuario e do sistema para tela
 
-//   console.log('Username:', username); 
-//   console.log('Password:', password);
-//   /*ver o lance de cifrar a senha */
-//   /*https://medium.com/collabcode/criptografando-e-descriptografando-dados-com-nodejs-f3f34a9390e4 */
-//   $.ajax({
-    
-//     url: '/login',
-//     method: 'POST',
-//     contentType: 'application/json',
-//     data:JSON.stringify({ username: username, password: password }),
-    
-//     success: function(response) {
-//       console.log('Entrou ajax ');
+    // Captura os elementos do HTML 
+    const messageInput = document.getElementById('message-input');
+    const sendButton = document.getElementById('send-button');
+    const chatContainer = document.getElementById('play-chat'); // Div onde o chat será exibido play-chat?   
+    sendButton.addEventListener('click', sendMessage);
+    var chatID = null;
 
-//       if (response.success) {
-//         // Login bem-sucedido, exibir o chat
-//         loginForm.reset(); // Limpa o formulário de login
-//         document.getElementById('login-screen').classList.add('hidden');
-//         chatScreen.classList.remove('hidden');
-//       } else {
-//         // Login inválido, exibir mensagem de erro
-//         alert(response.message);
-//       }
-//     },
-//     error: function() {
-//       alert('Ocorreu um erro durante a autenticação. Tente novamente');
-//       //console.log(JSON.stringify(error));
-//     }
-//   });  
-// });
+  function newChat(){
+    chatContainer.innerHTML = ''; // Limpar o conteúdo do elemento "play-chat"    
+    chatContainer.classList.add('hidden');
+    document.getElementById('sections').classList.remove('hidden');
+
+  }
+
+
+    function criarChat(message) {
+      return new Promise((resolve, reject) => {
+        
+       // console.log("user criar chat user id:", user_id);
+        nome_chat = message; // Substitua pelo nome do chat real
+    
+        // Fazer uma requisição AJAX para criar o chat
+        $.ajax({
+          url: '/criar-chat',
+          method: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({ user_id: user_id, nome_chat: nome_chat }),
+          success: function(response) {
+            // Verificar se a requisição foi bem-sucedida
+            if (response.success) {
+              chatID = response.chat_id;
+              console.log('Chat criado com sucesso');
+              resolve(); 
+            } else {
+              console.error('Erro ao criar o chat:', response.message);
+              reject(); 
+            }
+          },
+          error: function() {
+            console.error('Ocorreu um erro durante a criação do chat.');
+            reject(); 
+          }
+        });
+      });
+    }
+    
+    function exibirConteudoDoChat(chatId) {
+      // Fazer uma requisição AJAX para o servidor para obter o conteúdo do chat pelo ID
+      $.ajax({
+        url: `/obter-conteudo-chat/${chatId}`, 
+        method: 'GET',
+        success: function (response) {
+          // Verificar se a requisição foi bem-sucedida
+          if (response.success) {
+            chatID = chatId;
+            newChat() //limpar tudo antes de exibir garante que vai estar na tela "inicial" antes de exibir outro chat
+            document.getElementById('sections').classList.add('hidden');
+            chatContainer.classList.remove('hidden');
+            const conteudoDoChat = response.conteudo; // Conteúdo do chat obtido do servidor           
+
+            // Iterar sobre as mensagens do chat e exibi-las nas divs apropriadas
+            conteudoDoChat.forEach((messageObj) => {
+              console.log('objeto', messageObj);
+              const messageElement = document.createElement('p');
+              messageElement.textContent = messageObj.message;
+              
+              if (messageObj.is_system_message == 1){   
+                const systemdiv = document.createElement('div');
+                systemdiv.className = 'system-message';
+                systemdiv.appendChild(messageElement);
+                const imagem = document.createElement('img');
+                imagem.src = "images/bola_cristal.png";
+                systemdiv.appendChild(imagem);
+                chatContainer.appendChild(systemdiv);
+              }else{
+                //caso de mensagem ser do usuario        
+                const userdiv = document.createElement('div');
+                userdiv.className = 'user-message';     
+                const imagem = document.createElement('img');
+                imagem.src = profile_pic;
+                userdiv.appendChild(imagem);
+                userdiv.appendChild(messageElement);
+                chatContainer.appendChild(userdiv);
+              }             
+    
+            });
+          } else {
+            console.error('Erro ao obter o conteúdo do chat do servidor:', response.message);
+          }
+        },
+        error: function () {
+          console.error('Ocorreu um erro durante a requisição do conteúdo do chat.');
+        }
+      });
+    }
+    
+
+// Função para exibir os chats do usuário na lista
+
+function exibirChats(chats) {
+  const ulElement = document.querySelector('#old-chats ul');
+  ulElement.innerHTML = ''; // Limpar a lista antes de atualizar os chats
+
+  chats.forEach((chat) => {
+    const liElement = document.createElement('li');
+    const aElement = document.createElement('a');
+    aElement.href = '#';
+    aElement.textContent = chat.nome_chat;
+    // Adicionar o evento de clique para cada chat na lista
+    aElement.addEventListener('click', function() {
+      exibirConteudoDoChat(chat.chat_id); // Chamada da função que exibe o conteúdo do chat
+    });
+    liElement.appendChild(aElement);
+    ulElement.appendChild(liElement);
+  });
+}
+
+// Função para obter os chats do usuário
+function obterChatsDoUsuario(user_id) {
+  // Fazer uma requisição AJAX para o servidor 
+  $.ajax({
+    url: '/obter-chats', // Rota no servidor para obter os chats
+    method: 'GET',   
+    data:{ user_id: user_id },
+    
+    success: function(response) {
+      // Verificar se a requisição foi bem-sucedida
+      if (response.success) {
+        const chats = response.chats; // Chats obtidos do servidor
+        exibirChats(chats); // Atualizar a lista de chats no HTML
+      } else {
+        console.error('Erro ao obter os chats do servidor:', response.message);
+      }
+    },
+    error: function() {
+      console.error('Ocorreu um erro durante a requisição dos chats.');
+    }
+  });
+}
+
+var mensagemAleatoria = null;
+// Função para exibir uma mensagem aleatória do banco de dados
+function exibirMensagemAleatoria() {
+  return new Promise((resolve, reject) => {
+    // Fazer uma requisição AJAX para o servidor
+    $.ajax({
+      url: '/obter-mensagem-aleatoria', // Rota no servidor para obter uma mensagem aleatória
+      method: 'GET',
+      success: function(response) {
+        // Verificar se a requisição foi bem-sucedida
+        if (response.success) {
+          // Criar um elemento de parágrafo para exibir a mensagem
+          const messageElement = document.createElement('p');
+          messageElement.textContent = response.message; // A mensagem recebida do servidor
+          mensagemAleatoria = response.message;
+          const systemdiv = document.createElement('div');
+          systemdiv.className = 'system-message';
+
+          systemdiv.appendChild(messageElement);
+          const imagem = document.createElement('img');
+          imagem.src = "images/bola_cristal.png";
+          systemdiv.appendChild(imagem);
+          chatContainer.appendChild(systemdiv);           
+          resolve(response.message);
+        } else {
+          // Exibir uma mensagem de erro, caso necessário
+          console.error('Erro ao obter mensagem do servidor:', response.message);
+          
+          reject(new Error('Erro ao obter mensagem do servidor'));
+        }
+      },
+      error: function() {
+        // Exibir uma mensagem de erro, caso ocorra um erro na requisição
+        console.error('Ocorreu um erro durante a requisição da mensagem.');
+       
+        reject(new Error('Erro na requisição da mensagem'));
+      }
+    });
+  });
+}
+
+function salvarMensagemBD(chatId,message,user_id, is_system_message) {
+  // Fazer uma requisição AJAX para salvar a mensagem no banco de dados
+  $.ajax({
+    url: '/salvar-mensagem',
+    method: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({ message: message, chat_id: chatId, user_id:user_id, is_system_message: is_system_message }),
+    success: function(response) {
+      // Verificar se a requisição foi bem-sucedida
+      if (response.success) {
+        console.log('Mensagem salva com sucesso');
+      } else {
+        console.error('Erro ao salvar a mensagem:', response.message);
+      }
+    },
+    error: function() {
+      console.error('Ocorreu um erro ao salvar a mensagem.');
+    }
+  });
+}
+    
+    
+    // Função para enviar a mensagem
+    function sendMessage() {
+        const message = messageInput.value.trim(); // Obtém o valor da textarea e remove espaços em branco extras
+
+        if (message !== '') {
+            // Cria um novo elemento de parágrafo para exibir a mensagem
+            document.getElementById('sections').classList.add('hidden');
+            chatContainer.classList.remove('hidden');
+            const messageElement = document.createElement('p');
+            messageElement.textContent = message;
+            // Adiciona o elemento de mensagem ao chatContainer
+            
+            //mensagem do usuario
+            const userdiv = document.createElement('div');
+            userdiv.className = 'user-message';     
+            const imagem = document.createElement('img');
+            imagem.src = profile_pic;
+            userdiv.appendChild(imagem);
+            userdiv.appendChild(messageElement);
+            chatContainer.appendChild(userdiv);//div play-chat
+            
+            // Limpa o valor da textarea
+            messageInput.value = '';
+
+            // Verificar se é a primeira mensagem enviada pelo usuário
+            if (chatContainer.childElementCount === 1) {
+              criarChat(message)
+                .then(() => {
+                  salvarMensagemBD(chatID, message, user_id,false);
+                  obterChatsDoUsuario(user_id);
+                })
+                .catch(() => {
+                  console.error('Erro ao criar o chat');
+                });
+            } else {
+              salvarMensagemBD(chatID, message, user_id, false);
+            }        
+            
+             //pegar mensagem no banco de dados e exibir toda apos o usuario mandar mensagem
+             // Chamar a função para exibir a mensagem aleatória do banco de dados
+             console.log("mensagemAleato", mensagemAleatoria);
+             exibirMensagemAleatoria()
+              .then(()=>{
+                salvarMensagemBD(chatID,mensagemAleatoria,user_id,true);
+              })
+              .catch(()=>{
+                console.error('Erro ao exibir mensagem aleatoria');
+              })
+          
+           
+
+        }
+    }
+
